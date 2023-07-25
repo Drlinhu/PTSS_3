4070f7c13ac95212aa7109f402a48d5c4a7c500d
DrlinHu <drlinhulife@126.com>
2023-07-25 17:05:56 +0800

    initialize gui

---
+++ windows/main_window.py
@@ -0,0 +1,10 @@
+from PyQt5.QtWidgets import QMainWindow
+
+from .ui import Ui_MainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
